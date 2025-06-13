cc981171d34cee770fae0cfca5b66ea1b931e7c5
sk8dev <you@example.com>
2025-06-13 11:27:14 +0000

    synchronize tryporm prod

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -97,7 +97,8 @@ export class HealthController {
       password: process.env.DB_PASSWORD || 'postgres',
       database: process.env.DB_NAME || 'spa_comments',
       entities: [User, Post, Comment],
-      synchronize: process.env.NODE_ENV !== 'production',
+      synchronize: true, // Temporarily enable for first run
+      //synchronize: process.env.NODE_ENV !== 'production',
       logging:
         process.env.NODE_ENV !== 'production' ? ['error', 'warn'] : false,
       ssl:
